997cd5048ced241d405c0e98a018b8b6efd8cf40
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-05-19 18:31:14 +0300

    calculator: added dtos for offer generation

---
+++ calculator/src/main/java/steammachinist/dto/LoanOfferDto.java
@@ -0,0 +1,27 @@
+package steammachinist.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Schema(description = "Loan offer data")
+public record LoanOfferDto(
+        @Schema(description = "Unique offer UUID", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
+        UUID statementId,
+        @Schema(description = "Requested loan amount", example = "1500000")
+        BigDecimal requestedAmount,
+        @Schema(description = "Total loan offer amount", example = "1815000")
+        BigDecimal totalAmount,
+        @Schema(description = "Loan offer term", example = "24")
+        Integer term,
+        @Schema(description = "Loan offer monthly payment", example = "75625")
+        BigDecimal monthlyPayment,
+        @Schema(description = "Loan offer rate", example = "10")
+        BigDecimal rate,
+        @Schema(description = "Loan insurance enabled", example = "false")
+        Boolean isInsuranceEnabled,
+        @Schema(description = "Salary client", example = "true")
+        Boolean isSalaryClient
+) {
+}
---
+++ calculator/src/main/java/steammachinist/dto/LoanStatementRequestDto.java
@@ -0,0 +1,29 @@
+package steammachinist.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Schema(description = "Loan request data")
+public record LoanStatementRequestDto(
+        @Schema(description = "Requested loan amount", example = "1500000")
+        BigDecimal amount,
+        @Schema(description = "Requested loan term", example = "24")
+        Integer term,
+        @Schema(description = "User first name", example = "Иван")
+        String firstName,
+        @Schema(description = "User last name", example = "Иванов")
+        String lastName,
+        @Schema(description = "User middle name", example = "Иванович")
+        String middleName,
+        @Schema(description = "User email", example = "ivanov_i_1985@example.com")
+        String email,
+        @Schema(description = "User date of birth", example = "1985-05-12")
+        LocalDate birthdate,
+        @Schema(description = "User passport series", example = "2017")
+        String passportSeries,
+        @Schema(description = "User passport number", example = "927632")
+        String passportNumber
+) {
+}
