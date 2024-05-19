11fee052b7c32a2d6416e601b041d9fe10100ec0
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-05-19 18:32:57 +0300

    calculator: implemented initial CalculatorController

---
+++ calculator/src/main/java/steammachinist/CalculatorController.java
@@ -0,0 +1,29 @@
+package steammachinist;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import steammachinist.dto.CreditDto;
+import steammachinist.dto.LoanOfferDto;
+import steammachinist.dto.LoanStatementRequestDto;
+import steammachinist.dto.ScoringDataDto;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController implements CalculatorApi {
+
+    @Override
+    @PostMapping("/offers")
+    public Collection<LoanOfferDto> generatePossibleLoanOffers(LoanStatementRequestDto loanStatementRequestDto) {
+        return List.of();
+    }
+
+    @Override
+    @PostMapping("/calc")
+    public CreditDto calculateFullCreditData(ScoringDataDto scoringDataDto) {
+        return null;
+    }
+}
