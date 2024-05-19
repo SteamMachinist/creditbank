fdb3a298aacef13da2545b8f0ce1fe41e9171b0e
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-05-19 18:32:29 +0300

    calculator: implemented CalculatorApi interface

---
+++ calculator/src/main/java/steammachinist/CalculatorApi.java
@@ -0,0 +1,72 @@
+package steammachinist;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import steammachinist.dto.CreditDto;
+import steammachinist.dto.LoanOfferDto;
+import steammachinist.dto.LoanStatementRequestDto;
+import steammachinist.dto.ScoringDataDto;
+
+import java.util.Collection;
+
+@Tag(
+        name = "Calculator MS",
+        description = "MS for calculation of possible loan offers and full credit info"
+)
+public interface CalculatorApi {
+
+    @Operation(
+            summary = "Generate loan offers from request",
+            description = "Generates a list of possible loan offers from provided loan request",
+            requestBody = @RequestBody(
+                    description = "Loan request data to generate offers for",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = LoanStatementRequestDto.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful loan offers generation",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = LoanOfferDto.class)))
+                            }
+                    )
+            }
+    )
+    public Collection<LoanOfferDto> generatePossibleLoanOffers(LoanStatementRequestDto loanStatementRequestDto);
+
+    @Operation(
+            summary = "Conduct scoring and calculate full credit info",
+            description = "Conducts validation and scoring of provided scoring data, "
+                    + "calculates full credit information",
+            requestBody = @RequestBody(
+                    description = "Data for scoring and credit calculation",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ScoringDataDto.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful scoring and credit data calculation",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(
+                                                    schema = @Schema(implementation = CreditDto.class)))
+                            }
+                    )
+            }
+    )
+    public CreditDto calculateFullCreditData(ScoringDataDto scoringDataDto);
+}
