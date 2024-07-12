eb6142f2088a108fec04612668621cfb4e00ea4b
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-07-12 23:16:07 +0300

    deal: implement service to call calculator api

---
+++ deal/src/main/java/ru/neoflex/deal/service/CalculatorApiService.java
@@ -0,0 +1,60 @@
+package ru.neoflex.deal.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.neoflex.calculator.dto.offer.request.LoanStatementRequestDto;
+import ru.neoflex.calculator.dto.offer.response.LoanOfferDto;
+import ru.neoflex.calculator.dto.scoring.request.ScoringDataDto;
+import ru.neoflex.calculator.dto.scoring.response.CreditDto;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CalculatorApiService {
+
+    @Value("${deal.calculator-base-url}")
+    private String calculatorApiUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public List<LoanOfferDto> getPossibleOffers(LoanStatementRequestDto loanStatementRequestDto) {
+        String url = calculatorApiUrl + "/offers";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<LoanStatementRequestDto> request = new HttpEntity<>(loanStatementRequestDto, headers);
+
+        ResponseEntity<LoanOfferDto[]> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                request,
+                LoanOfferDto[].class
+        );
+
+        return Arrays.asList(response.getBody());
+    }
+
+    public CreditDto getFullCreditData(ScoringDataDto scoringDataDto) {
+        String url = calculatorApiUrl + "/calc";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<ScoringDataDto> request = new HttpEntity<>(scoringDataDto, headers);
+
+        ResponseEntity<CreditDto> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                request,
+                CreditDto.class
+        );
+
+        return response.getBody();
+    }
+}
