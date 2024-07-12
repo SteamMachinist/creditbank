25607d3d9e09c8c0258852e404a092e8bdc09a45
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-07-13 02:20:52 +0300

    deal: fix absent uuid generation

--- deal/src/main/java/ru/neoflex/deal/entity/ClientListener.java
+++ deal/src/main/java/ru/neoflex/deal/entity/ClientListener.java
@@ -10,12 +10,12 @@ public class ClientListener {
     @PreUpdate
     public void setInternalUuidsIfAbsent(Client client) {
         Passport passport = client.getPassport();
-        if (passport != null && passport.passportUUID() != null) {
+        if (passport != null && passport.passportUUID() == null) {
             client.setPassport(passport.withPassportUUID(UUID.randomUUID()));
         }
 
         Employment employment = client.getEmployment();
-        if (employment != null && employment.employmentUUID() != null) {
+        if (employment != null && employment.employmentUUID() == null) {
             client.setEmployment(employment.withEmploymentUUID(UUID.randomUUID()));
         }
     }
