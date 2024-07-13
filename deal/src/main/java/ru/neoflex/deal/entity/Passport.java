4904cb806739c85c2320d4d232ed7943d3b801bb
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-07-13 20:47:05 +0300

    deal: added with method for Passport update

--- deal/src/main/java/ru/neoflex/deal/entity/Passport.java
+++ deal/src/main/java/ru/neoflex/deal/entity/Passport.java
@@ -1,22 +1,31 @@
 package ru.neoflex.deal.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.With;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Builder
 public record Passport(
 
-        @JsonProperty("passport_uuid")
         @With
+        @JsonProperty("passport_uuid")
         UUID passportUUID,
+
         String series,
         String number,
+
         @With
         @JsonProperty("issue_branch")
         String issueBranch,
+
         @With
         @JsonProperty("issue_date")
         LocalDate issueDate) {
+
+        public Passport withIssueBranchAndIssueDate(String issueBranch, LocalDate issueDate) {
+                return this.withIssueBranch(issueBranch).withIssueDate(issueDate);
+        }
 }
