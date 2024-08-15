043665f0980c15df874539404a36ee63024b98ac
Yaroslav Roshchupkin <roshchupkin.ya@gmail.com>
2024-08-15 19:43:53 +0300

    dossier: implement topics properties usage

---
+++ deal/src/main/java/ru/neoflex/deal/configuration/KafkaTopic.java
@@ -0,0 +1,11 @@
+package ru.neoflex.deal.configuration;
+
+public enum KafkaTopic {
+
+    FINISH_REGISTRATION,
+    CREATE_DOCUMENTS,
+    SEND_DOCUMENTS,
+    SEND_SES,
+    CREDIT_ISSUED,
+    STATEMENT_DENIED
+}
