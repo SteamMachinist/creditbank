b1dd2de7df7fe6562b68505c30680c8d3d55db9c
Yaroslav Roshchupkin <roshchupkin.ya@gmail.com>
2024-08-18 00:41:40 +0300

    dossier: add logging

---
+++ dossier/src/main/java/ru/neoflex/dossier/logging/LoggingAspect.java
@@ -0,0 +1,35 @@
+package ru.neoflex.dossier.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut("execution(* ru.neoflex.deal.service.*.*(..)) ")
+    public void combinedPointcut() {
+    }
+
+    @Before("combinedPointcut()")
+    public void logMethodCall(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        log.info("Method {} called with arguments: {}", methodName, args);
+    }
+
+    @AfterReturning(pointcut = "combinedPointcut()", returning = "result")
+    public void logMethodReturn(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Method {} returned with result: {}", methodName, result);
+    }
+
+    @AfterThrowing(pointcut = "combinedPointcut()", throwing = "exception")
+    public void logMethodException(JoinPoint joinPoint, Throwable exception) {
+        String methodName = joinPoint.getSignature().getName();
+        log.error("Method {} thrown exception: {}", methodName, exception.getMessage());
+    }
+}
