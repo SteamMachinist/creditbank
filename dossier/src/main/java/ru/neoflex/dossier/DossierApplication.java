ca319c590dabf57749256e8b40ceee3af55b5186
Yaroslav Roshchupkin <roshchupkin.ya@gmail.com>
2024-08-18 00:38:46 +0300

    dossier: added topic properties

--- dossier/src/main/java/ru/neoflex/dossier/DossierApplication.java
+++ dossier/src/main/java/ru/neoflex/dossier/DossierApplication.java
@@ -1,9 +1,12 @@
-package ru.neoflex;
+package ru.neoflex.dossier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.neoflex.dossier.configuration.KafkaTopicsProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = KafkaTopicsProperties.class)
 public class DossierApplication {
 
     public static void main(String[] args) {
